0870c861f40688ffee18d8a6fffdce2cccd1f389
Jason <jason.ricou@gmail.com>
2023-04-08 16:39:41 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,7 @@ require ('dotenv').config();
 
 const api = process.env.NPMJS_API_KEY;
 
-router.use((req, res, next) => {
-    res.status(404).json({ message: "La page demandée n'a pas été trouvée" });
-  });
 //requête vers le webservice The Movie Database afin de récupérer les dernières "découvertes"
-
 router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api}`)
     .then(response =>response.json())
