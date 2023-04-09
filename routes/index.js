fe86f338d9b14b3005b20de0682a2fb661dcf572
Jason <jason.ricou@gmail.com>
2023-04-09 11:06:28 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,27 @@
-
 var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch')
-require ('dotenv').config();
+require('dotenv').config();
 
 const api = process.env.NPMJS_API_KEY;
 
 //requête vers le webservice The Movie Database afin de récupérer les dernières "découvertes"
 router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api}`)
-    .then(response =>response.json())
-    .then(apiData=> {
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur 404 : la ressource est introuvable');
+        }
+        return response.json();
+    })
+    .then(apiData => {
         console.log('fetch', apiData.page)
         res.json(apiData.results);  
     })
-    .catch((error) => console.log("error", error))
+    .catch((error) => {
+        console.log(error.message);
+        res.status(404).send(error.message);
+    });
 })
-
-
 
 module.exports = router;
